43af6e10d703f0c5c0c818723c6485550f398247
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-03-15 06:20:51 +0000

    Fix PayedService Form.

--- lib/Form/PayedServiceForm.php
+++ lib/Form/PayedServiceForm.php
@@ -5,6 +5,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\HttpFoundation\RequestStack;
 
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
--- lib/Form/Type/PayedServiceSubscriptionPeriodType.php
+++ lib/Form/Type/PayedServiceSubscriptionPeriodType.php
@@ -3,8 +3,9 @@
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 use Vankosoft\UsersSubscriptionsBundle\Model\PayedServiceSubscriptionPeriod;
 
