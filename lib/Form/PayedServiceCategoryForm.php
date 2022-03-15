7284ad84b6985437d91ddbb40c8cc76ea9212a2a
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-03-15 05:56:24 +0000

    Fix.

--- lib/Form/PayedServiceCategoryForm.php
+++ lib/Form/PayedServiceCategoryForm.php
@@ -27,13 +27,10 @@ class PayedServiceCategoryForm extends AbstractForm
     {
         parent::buildForm( $builder, $options );
         
-        $entity         = $builder->getData();
-        $currentLocale  = $entity->getLocale() ?: $this->requestStack->getCurrentRequest()->getLocale();
-        
         $builder
             ->add( 'currentLocale', ChoiceType::class, [
                 'choices'               => \array_flip( I18N::LanguagesAvailable() ),
-                'data'                  => $currentLocale,
+                'data'                  => $this->requestStack->getCurrentRequest()->getLocale(),
                 'mapped'                => false,
                 'label'                 => 'vs_users_subscriptions.form.locale',
                 'translation_domain'    => 'VSUsersSubscriptionsBundle',
