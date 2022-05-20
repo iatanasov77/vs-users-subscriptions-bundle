f99be841b2f752344acc1649e64ee995069df7f8
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-05-20 16:16:45 +0000

    Detach this bundle from PaymentBundle.

--- lib/Model/Interfaces/SubscribedUserInterface.php
+++ lib/Model/Interfaces/SubscribedUserInterface.php
@@ -2,8 +2,9 @@
 
 use Symfony\Component\Security\Core\User\UserInterface as BaseUserInterface;
 use Sylius\Component\Resource\Model\ResourceInterface;
+use Doctrine\Common\Collections\Collection;
 
 interface SubscribedUserInterface extends BaseUserInterface, ResourceInterface
 {
-
+    public function getSubscriptions(): Collection;
 }
