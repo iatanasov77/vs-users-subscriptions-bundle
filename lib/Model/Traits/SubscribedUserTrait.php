cacf7bf93da318ab41203817da9a74a66db70b47
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-06-11 19:58:21 +0000

    Fix and Improve SubscribedUserTrait.

--- lib/Model/Traits/SubscribedUserTrait.php
+++ lib/Model/Traits/SubscribedUserTrait.php
@@ -1,5 +1,6 @@
 <?php namespace Vankosoft\UsersSubscriptionsBundle\Model\Traits;
 
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 
@@ -7,39 +8,101 @@ use Vankosoft\UsersSubscriptionsBundle\Model\Interfaces\SubscriptionInterface;
 
 trait SubscribedUserTrait
 {
+    const SUBSCRIPTION_TYPE_NEWSLETTER  = 'newsletter_subscriptions';
+    const SUBSCRIPTION_TYPE_PAID        = 'paid_service_subscriptions';
+    
     /**
      * @var Collection|SubscriptionInterface[]
+     *
+     * @ORM\OneToMany(targetEntity="Vankosoft\UsersSubscriptionsBundle\Model\Interfaces\NewsletterSubscriptionInterface", mappedBy="user")
      */
-    protected $subscriptions;
+    protected $newsletterSubscriptions;
+    
+    /**
+     * @var Collection|SubscriptionInterface[]
+     * 
+     * @ORM\OneToMany(targetEntity="Vankosoft\UsersSubscriptionsBundle\Model\Interfaces\PayedServiceSubscriptionInterface", mappedBy="user")
+     */
+    protected $paidSubscriptions;
     
     /**
      * @return Collection|SubscriptionInterface[]
      */
-    public function getSubscriptions(): Collection
+    public function getSubscriptions( $type ): Collection
     {
-        return $this->subscriptions;
+        switch ( $type ) {
+            case self::SUBSCRIPTION_TYPE_NEWSLETTER:
+                return $this->newsletterSubscriptions;
+                break;
+            case self::SUBSCRIPTION_TYPE_PAID:
+                return $this->paidSubscriptions;
+                break;
+            default:
+                throw new \Exception( 'Undefined Subscription Type !!!' );
+        }
     }
     
-    public function setSubscriptions( Collection $subscriptions ): self
+    /**
+     * @return Collection|SubscriptionInterface[]
+     */
+    public function getNewsletterSubscriptions(): Collection
     {
-        $this->subscriptions  = $subscriptions;
+        return $this->newsletterSubscriptions;
+    }
+    
+    public function setNewsletterSubscriptions( Collection $newsletterSubscriptions ): self
+    {
+        $this->newsletterSubscriptions  = $newsletterSubscriptions;
         
         return $this;
     }
     
-    public function addSubscription( SubscriptionInterface $subscription ): self
+    public function addNewsletterSubscription( SubscriptionInterface $newsletterSubscription ): self
     {
-        if ( ! $this->subscriptions->contains( $subscription ) ) {
-            $this->subscriptions[]    = $subscription;
+        if ( ! $this->newsletterSubscriptions->contains( $newsletterSubscription ) ) {
+            $this->newsletterSubscriptions[]    = $newsletterSubscription;
         }
         
         return $this;
     }
     
-    public function removeSubscription( SubscriptionInterface $subscription ): self
+    public function removeNewsletterSubscription( SubscriptionInterface $newsletterSubscription ): self
     {
-        if ( $this->subscriptions->contains( $subscription ) ) {
-            $this->subscriptions->removeElement( $subscription );
+        if ( $this->newsletterSubscriptions->contains( $newsletterSubscription ) ) {
+            $this->newsletterSubscriptions->removeElement( $newsletterSubscription );
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * @return Collection|SubscriptionInterface[]
+     */
+    public function getPaidSubscriptions(): Collection
+    {
+        return $this->paidSubscriptions;
+    }
+    
+    public function setPaidSubscriptions( Collection $paidSubscriptions ): self
+    {
+        $this->paidSubscriptions  = $paidSubscriptions;
+        
+        return $this;
+    }
+    
+    public function addPaidSubscription( SubscriptionInterface $paidSubscription ): self
+    {
+        if ( ! $this->paidSubscriptions->contains( $paidSubscription ) ) {
+            $this->paidSubscriptions[]    = $paidSubscription;
+        }
+        
+        return $this;
+    }
+    
+    public function removePaidSubscription( SubscriptionInterface $paidSubscription ): self
+    {
+        if ( $this->paidSubscriptions->contains( $paidSubscription ) ) {
+            $this->paidSubscriptions->removeElement( $paidSubscription );
         }
         
         return $this;
