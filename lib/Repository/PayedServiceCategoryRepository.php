987bbb1b1072bcd330413d19f3286d7e4c6e5b5d
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-09-23 07:02:05 +0000

    Add PayedServiceCategoryRepository .

---
+++ lib/Repository/PayedServiceCategoryRepository.php
@@ -0,0 +1,8 @@
+<?php namespace Vankosoft\UsersSubscriptionsBundle\Repository;
+
+use Vankosoft\ApplicationBundle\Repository\TaxonDescendentRepository;
+
+class PayedServiceCategoryRepository extends TaxonDescendentRepository
+{
+    
+}
